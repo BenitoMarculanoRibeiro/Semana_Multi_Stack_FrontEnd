7de6982c1acb138e379fb0bdfb8c07b524b75159
BenitoMarculanoRibeiro <benito2016mr@gmail.com>
2022-06-05 18:19:16 -0300

    0.0.4 - Fim aula 3.

--- src/ui/components/Lista/Lista.tsx
+++ src/ui/components/Lista/Lista.tsx
@@ -4,29 +4,32 @@ import { Pet } from '../../../data/@types/Pet'
 import { TextService } from '../../../data/services/TextService'
 interface ListaProps {
     pets: Pet[];
+    onSelect: (pet: Pet) => void;
 }
 
 
 export default function Lista(props: ListaProps) {
     const tamanhoMaximoTexto = 200;
     return (
-        <div>
-            <ListaStyled>
-                {props.pets.map(pet => (
-                    <ItemLista key={pet.id}>
-                        <Foto src={pet.foto} alt={pet.nome} />
-                        <Informacoes>
-                            <Nome>{pet.nome}</Nome>
-                            <Descricao>
-                                {TextService.limitarTexto(pet.historia, tamanhoMaximoTexto)}
-                            </Descricao>
-                            <Button variant={'contained'} fullWidth>
-                                Adotar {pet.nome}
-                            </Button>
-                        </Informacoes>
-                    </ItemLista>
-                ))}
-            </ListaStyled>
-        </div>
+        <ListaStyled>
+            {props.pets.map(pet => (
+                <ItemLista key={pet.id}>
+                    <Foto src={pet.foto} alt={pet.nome} />
+                    <Informacoes>
+                        <Nome>{pet.nome}</Nome>
+                        <Descricao>
+                            {TextService.limitarTexto(pet.historia, tamanhoMaximoTexto)}
+                        </Descricao>
+                        <Button
+                            variant={'contained'}
+                            fullWidth
+                            onClick={() => props.onSelect(pet)}
+                        >
+                            Adotar {pet.nome}
+                        </Button>
+                    </Informacoes>
+                </ItemLista>
+            ))}
+        </ListaStyled>
     )
 }
